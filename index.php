5fdb0312e54818bd62c29d52ad704f3fb58d55f6
Johannes Nilsson <johannes@nilsson.im>
2020-09-23 14:04:24 +0200

    Add index w headers

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/* DTG173G - Moment 5
+ * Johannes Nilsson, HT20
+ */
+
+// Set CORS headers
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
