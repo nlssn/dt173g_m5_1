f5f1bf4b3a32b7b3e2b82b2d9134820751b21cd3
Johannes Nilsson <johannes@nilsson.im>
2020-09-23 14:05:24 +0200

    Add helper vars

--- index.php
+++ index.php
@@ -8,3 +8,11 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+// Save the request method for later use
+$method = $_SERVER['REQUEST_METHOD'];
+
+// If a param of ID is set, save that too
+if(isset($_GET['id'])) {
+   $id = $_GET['id'];
+}
