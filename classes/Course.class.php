eed01b2efb7728be96fbf105a619e2e9b7e510c1
Johannes Nilsson <johannes@nilsson.im>
2020-09-23 15:10:39 +0200

    Create response array in method

--- classes/Course.class.php
+++ classes/Course.class.php
@@ -20,10 +20,33 @@ class Course {
 
    // READ
    public function read() {
-      $query = 'SELECT id, name, code, progression, syllabus FROM ' . $this->table;
+      $query = 'SELECT * FROM ' . $this->table;
       $stmt = $this->conn->prepare($query);
       $stmt->execute();
-      return $stmt;
-   }
+      $num = $stmt->rowCount();
 
+      if ($num > 0) {
+         $courses = array();
+         $courses['data'] = array();
+         $courses['itemCount'] = $num;
+
+         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            extract($row); // extract data from row as variables
+
+            $c = array(
+               'id' => $id,
+               'name' => $name,
+               'code' => $code,
+               'progression' => $progression,
+               'syllabus' => $syllabus
+            );
+
+            array_push($courses['data'], $c);
+         }
+
+         return $courses;
+      } else {
+         return array('message' => 'Inga kurser hittades');
+      }
+   }
 }
